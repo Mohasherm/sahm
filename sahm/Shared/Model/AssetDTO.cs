d41b66a984fd45bb2ba99879fb340075120e81fa
Mohabek <Mohabek@MH.com>
2023-05-25 00:27:19 +0200

    res

--- sahm/Shared/Model/AssetDTO.cs
+++ sahm/Shared/Model/AssetDTO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LocalizationLibrary.Resources;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,7 @@ namespace sahm.Shared.Model
     public class AssetDTO
     {
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
     }
