005bbe049b1c5f9142f78dd3e84ded86ae3e3a53
Mohammad Sherm <87722765+Mohasherm@users.noreply.github.com>
2023-05-25 17:49:51 +0200

    added jobTitles Pages

--- sahm/Shared/Model/TankQTYUpdateDTO.cs
+++ sahm/Shared/Model/TankQTYUpdateDTO.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace sahm.Shared.Model
 {
     public class TankQTYUpdateDTO
     {
         public int Id { get; set; }
+        [Required]
         public double QTY { get; set; }
         public string OperationType { get; set; } = string.Empty;
     }
